85d768311c840bd62bafcc655104ff61f86ac340
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-24 17:39:35 +0000

    parser -- branch 2.0 -- XmlReader new

--- Parser.php
+++ Parser.php
@@ -10,7 +10,7 @@ use Psr\Container\ContainerInterface;
  * @desc Extension PresstiFy de traitement, conversion et écriture de données au format CSV,Excel,JSON,XML.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy\Plugins\Parser
- * @version 2.0.2
+ * @version 2.0.3
  *
  * USAGE :
  * Activation
