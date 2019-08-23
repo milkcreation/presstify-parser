dccd93c01b0b65d350098db98c06d7a8005df821
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-08-23 17:15:45 +0000

    parser -- tag 2.0.5 -- Gestion des log + Réorganisation dans Parsers + Réécriture globale + Template FileListTable

---
+++ parser/tags/2.0.5/ParserManager.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Parser;
+
+use tiFy\Plugins\Parser\Contracts\ParserManager as ParserManagerContract;
+use tiFy\Support\Manager;
+
+/**
+ * @desc Extension PresstiFy de traitement, conversion et écriture de données au format CSV,Excel,JSON,LOG,XML.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\Parser
+ * @version 2.0.5
+ *
+ * USAGE :
+ * Activation
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\Parser\ParserServiceProvider à la liste des fournisseurs de services.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\Parser\ParserServiceProvider;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          ParserServiceProvider::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier parser.php
+ * @see Resources/config/parser.php
+ */
+class ParserManager extends Manager implements ParserManagerContract
+{
+
+}
---
+++ parser/tags/2.0.5/ParserServiceProvider.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Parser;
+
+use tiFy\Container\ServiceProvider;
+use tiFy\Plugins\Parser\Template\FileListTable\{
+    Contracts\FileBuilder as FileListTableFileBuilderContract,
+    FileBuilder as FileListTableFileBuilder
+};
+
+class ParserServiceProvider extends ServiceProvider
+{
+    /**
+     * Liste des noms de qualification des services fournis.
+     * @internal requis. Tous les noms de qualification de services à traiter doivent être renseignés.
+     * @var string[]
+     */
+    protected $provides = [
+        'parser'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function boot()
+    {
+        add_action('after_setup_theme', function() {
+            $this->getContainer()->get('parser');
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function register()
+    {
+        $this->getContainer()->share('parser', function() {
+            return new ParserManager($this->getContainer());
+        });
+        $this->registerImportListTable();
+    }
+
+    /**
+     * Déclaration du template d'import.
+     *
+     * @return void
+     */
+    public function registerImportListTable(): void
+    {
+        $this->getContainer()->add(FileListTableFileBuilderContract::class, function () {
+            return new FileListTableFileBuilder();
+        });
+    }
+}
