f568311b38fd17b44cbd4cd17602501a04ac53d4
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-05-29 14:25:23 +0000

    parse -- 2.0.22 -- Maj Lib Outdated + retour compat XML

---
+++ parser/tags/2.0.22/Parsers/JsonFileParser.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Parser\Parsers;
+
+use Exception;
+use tiFy\Plugins\Parser\{
+    Contracts\FileParser as FileParserContract,
+    Contracts\JsonFileParser as JsonFileParserContract,
+    FileParser
+};
+use JsonCollectionParser\Listener;
+use JsonStreamingParser\Parser as StreamingParser;
+
+class JsonFileParser extends FileParser implements JsonFileParserContract
+{
+    /**
+     * @inheritDoc
+     */
+    public function parse(): FileParserContract
+    {
+        try {
+            $this->stream = $this->open();
+
+           (new StreamingParser(
+               $this->stream,
+               new Listener(function(array $item) {
+                   $this->records[] = $item;
+               },
+               true
+               )
+           ))->parse();
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        if( ! fclose($this->stream)){
+            throw new Exception();
+        }
+
+        return $this;
+    }
+}
