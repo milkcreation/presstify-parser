a786eee0fa1bb60e9cc3b56fc1aa88253cefce68
5upp0r7 <5upp0r7@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-09-25 16:50:45 +0000

    parser v2.0.26 > branch

---
+++ parser/tags/2.0.26/CHANGELOG.md
@@ -0,0 +1,15 @@
+# Release Notes
+
+## [v2.0.26 (2020-09-25)](https://svn.tigreblanc.fr/presstify-plugins/parser/tags/2.0.26...v2.0.26)
+
+### Fixed
+
+- Parsers >> Driver
+- Prise en compte de du paramètre "primary" en callable
+
+
+## [v2.0.25 (2020-09-12)](https://svn.tigreblanc.fr/presstify-plugins/parser/tags/2.0.25...v2.0.25)
+
+### Fixed
+
+- `Driver/CsvFileParser.php`: Prise en compte du header type array
---
+++ parser/tags/2.0.26/Reader.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\Parser;
+
+use Exception;
+use tiFy\Plugins\Parser\{
+    Contracts\Reader as ReaderContract,
+    Exceptions\ReaderException,
+    Driver\CsvReader,
+    Driver\JsonReader,
+    Driver\LogReader,
+    Driver\XmlReader
+};
+
+/**
+ * USAGE :
+ * ---------------------------------------------------------------------------------------------------------------------
+ use tiFy\Plugins\Parser\Reader;
+
+ $reader = Reader::createFromPath(
+    VENDOR_PATH . '/presstify-plugins/parser/Resources/sample/sample.json', [
+    'offset'        => 1,
+    'primary'       => 'lastname',
+    'page'          => 1,
+    'per_page'      => -1
+ ]);
+
+ // Lancement de la récupération des éléments.
+ // @var \tiFy\Plugins\Contracts\Reader
+ $reader->fetch();
+
+ // Récupération du tableau de la liste des éléments courants.
+ // @var array
+ $reader->all();
+
+ // Récupération de la liste des complète des enregistrements.
+ // @var array
+ $reader->getRecords();
+
+ // Récupération la liste des éléments de la page 2.
+ // @var array
+ $reader->fetchForPage(2)->all();
+
+ // Récupération du nombre total de resultats.
+ // @var int
+ $reader->getTotal();
+
+ // Récupération du numéro de la dernière de page.
+ // @var int
+ $reader->getLastPage();
+
+ // Récupération du nombre d'éléments courants.
+ // @var int
+ $reader->getCount();
+ */
+class Reader extends AbstractReader implements ReaderContract
+{
+    /**
+     * @inheritDoc
+     *
+     * @throws ReaderException
+     */
+    public static function createFromPath(string $path, array $params = [], ...$args): ReaderContract
+    {
+        try {
+            switch ($ext = pathinfo($path, PATHINFO_EXTENSION)) {
+                default:
+                    throw new ReaderException(__('Le type de fichier n\'est pas pris en charge.', 'tify'));
+                    break;
+                case 'csv' :
+                    return CsvReader::createFromPath($path, $params, ...$args);
+                    break;
+                case 'json' :
+                    return JsonReader::createFromPath($path, $params, ...$args);
+                    break;
+                case 'log' :
+                    return LogReader::createFromPath($path, $params, ...$args);
+                    break;
+                case 'xml' :
+                    return XmlReader::createFromPath($path, $params, ...$args);
+                    break;
+            }
+        } catch (Exception $e) {
+            throw new ReaderException(__('Impossible de lire le fichier.', 'tify'));
+        }
+    }
+}
