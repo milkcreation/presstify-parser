f39d2fbdcb1dac8f68e4ab697358646e109ca09d
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-11-10 07:34:10 +0000

    parser v2.0.28 > tag

---
+++ parser/tags/2.0.28/Resources/ide/Autocomplete.php
@@ -0,0 +1,14 @@
+<?php
+namespace  {
+    exit("This file should not be included, only analyzed by your IDE");
+}
+
+namespace Tightenco\Collect\Support {
+    /**
+     * @method Collection recursiveToArray()
+     */
+    class Collection
+    {
+
+    }
+}
